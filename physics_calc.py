1022dca517929b6a68f04dab168a236a3bdd586b
Atharva-TheCoder <73903228+Atharva-TheCoder@users.noreply.github.com>
2020-12-09 13:39:11 +0530

    Update physics_calc.py

--- physics_calc.py
+++ physics_calc.py
@@ -32,23 +32,23 @@ while True:
           x = int(input("Displacement(x) : "))
           t = int(input("Time(t) : "))
           res = x/t
-          print (lgreen+"[Result] = "+lgreen , yellow+res , "m/s"+yellow)
+          print ("[Result] = ", res , "m/s")
        elif choice == 2:
           t = int(input("Time(t) : "))
           v = int(input("Velocity(v) : "))
           res = v*t
-          print ("Result = ", res , "m")
+          print ("[Result] = ", res , "m")
        elif choice == 3:
           x = int(input("Displacement(x) : "))
           v = int(input("Velocity(v) : "))
           res = x/v
-          print ("Result = ", res , "s")
+          print ("[Result] = ", res , "s")
        else:
           v = int(input("Velocity(v) : "))
           t = int(input("Time(t) : "))
           res = v/t
-          print ("Result = ", res , "m/s²")
+          print ("[Result] = ", res , "m/s²")
     elif choice == 5:
        exit()
     else:
-       print ("Wrong input..!!")
+       print (red+"Wrong input..!!"+red)
