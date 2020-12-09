ce45897e1b5fe7c6464474589a7ec1e28ce4a5b3
Atharva-TheCoder <73903228+Atharva-TheCoder@users.noreply.github.com>
2020-12-09 15:20:41 +0530

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,48 @@
+#colour section
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+#script coding starts
+clear
+echo " "
+echo " "
+echo '
+                  _  _  ___  ___   __  ____  ___
+                 ( )( )(  ,\(   \ (  )(_  _)(  _)
+                  )()(  ) _/ ) ) )/__\  )(   ) _)
+                  \__/ (_)  (___/(_)(_)(__) (___) v 1.0
+'|lolcat
+echo " "
+echo " "
+sleep 4.0
+clear
+echo -e "$red                         Checking..>$rset"
+sleep 1.0
+clear
+echo -e "$red                         Checking...>$rset"
+sleep 1.0
+clear
+echo -e "$red                         Checking....>$rset"
+sleep 1.0
+clear
+echo -e "$red                         Checking.....>$rset"
+sleep 1.0
+clear
+echo " "
+echo " "
+echo -e "$grn                Updating the calculator please wait$rset"
+sleep 2.0
+cd $HOME
+rm -rf Physics-Calculator 
+git clone https://github.com/Atharva-TheCoder/Physics-Calculator  
+clear
+echo " "
+echo -e "$grn               Calculator has been updated check it now...$rset"
+sleep 3.0
+echo " "
+cd $HOME
+cd Physics-Calculator 
+python physics_calc.py
+clear
+sleep 1.0 
