85f47342f442f1c14ef8c43a19772430e0246cfe
Atharva-TheCoder <73903228+Atharva-TheCoder@users.noreply.github.com>
2020-12-09 13:21:41 +0530

    Update physics_calc.py

--- physics_calc.py
+++ physics_calc.py
@@ -13,7 +13,7 @@ print (yellow+"""
 (  _ \/ )( \( \/ )/ ___)(  )/ __)/ ___)  / __) / _\ (  )   / __)
  ) __/) __ ( )  / \___ \ )(( (__ \___ \ ( (__ /    \/ (_/\( (__ 
 (__)  \_)(_/(__/  (____/(__)\___)(____/  \___)\_/\_/\____/ \___) """+yellow) 
-print (lgreen+"""                                                      (v 1.0)"""+lgreen)
+print (lgreen+"""                                                        (v 1.0)"""+lgreen)
                                                          
 print (lgreen+bold+"          <===[[ coded by @tharva-TheHacker ]]===> \n"+clear)
 
